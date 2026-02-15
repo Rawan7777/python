b72e87e320b740fa762a3b4faac42b68863a870e
bilalrouane1337 <bilal.rouane1337@gmail.com>
2026-02-15 23:01:27 +0000

    push

--- python03/ex3/ft_achievement_tracker.py
+++ python03/ex3/ft_achievement_tracker.py
@@ -52,39 +52,39 @@ data = {
     ]
 }
 
-Player_alice_achievements = set(data["alice"])
-Player_bob_achievements = set(data["bob"])
-Player_charlie_achievements = set(data["charlie"])
+alice_achiev = set(data["alice"])
+bob_achiev = set(data["bob"])
+charlie_achiev = set(data["charlie"])
 
 print("=== Achievement Tracker System ===\n")
 
-print(f"Player alice achievements: {Player_alice_achievements}")
-print(f"Player bob achievements: {Player_bob_achievements}")
-print(f"Player charlie achievements: {Player_charlie_achievements}")
+print(f"Player alice achievements: {alice_achiev}")
+print(f"Player bob achievements: {bob_achiev}")
+print(f"Player charlie achievements: {charlie_achiev}")
 
 print("\n=== Achievement Analytics ===")
 
-union_set = Player_alice_achievements.union(Player_bob_achievements).union(Player_charlie_achievements)
+union_set = alice_achiev.union(bob_achiev).union(charlie_achiev)
 print(f"All unique achievements: {union_set}")
 print(f"Total unique achievements: {len(union_set)}\n")
 
-intersiction_set = Player_alice_achievements.intersection(Player_bob_achievements).intersection(Player_charlie_achievements)
+intersiction_set = alice_achiev.intersection(bob_achiev).intersection(charlie_achiev)
 print(f"Common to all players: {intersiction_set}")
 
-bob_difference_set = Player_bob_achievements.difference(Player_charlie_achievements)
-charlie_difference_set = Player_charlie_achievements.difference(Player_bob_achievements)
+bob_difference_set = bob_achiev.difference(charlie_achiev)
+charlie_difference_set = charlie_achiev.difference(bob_achiev)
 
 difference_set = bob_difference_set
 difference_set.update(charlie_difference_set)
 
-difference_sett = difference_set.difference(Player_alice_achievements)
+difference_sett = difference_set.difference(alice_achiev)
 print(f"Rare achievements (1 player): {difference_sett}\n")
 
-alice_vs_bob = Player_alice_achievements.intersection(Player_bob_achievements)
+alice_vs_bob = alice_achiev.intersection(bob_achiev)
 print(f"Alice vs Bob common: {alice_vs_bob}")
 
-alice_unique = Player_alice_achievements.difference(Player_bob_achievements)
+alice_unique = alice_achiev.difference(bob_achiev)
 print(f"Alice unique:: {alice_unique}")
 
-bob_unique = Player_bob_achievements.difference(Player_alice_achievements)
+bob_unique = bob_achiev.difference(alice_achiev)
 print(f"Bob unique: {bob_unique}")
