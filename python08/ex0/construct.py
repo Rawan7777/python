0ec5bf0463b8434a001303c667e5901a28d5b332
RowanSe7en <bilal.rouane702@gmail.com>
2026-04-04 15:29:18 +0100

    are you real

--- python08/ex0/construct.py
+++ python08/ex0/construct.py
@@ -10,7 +10,7 @@ def main() -> None:
 
     python_path = sys.executable
 
-    print("MATRIX STATUS:", "Welcome to the construct" if in_virtualenv() else "You're still plugged in")
+    print("MATRIX STATUS:", "Welcome to the construct\n" if in_virtualenv() else "You're still plugged in\n")
     print(f"Current Python: {python_path}")
 
     if in_virtualenv():
@@ -18,21 +18,22 @@ def main() -> None:
         venv_name = os.path.basename(sys.prefix)
 
         print(f"Virtual Environment: {venv_name}")
-        print(f"Environment Path: {sys.prefix}")
+        print(f"Environment Path: {sys.prefix}\n")
         print("SUCCESS: You're in an isolated environment!")
-        print("Safe to install packages without affecting the global system.")
+        print("Safe to install packages without affecting the global system.\n")
         print("Package installation path:")
         print(site.getsitepackages()[0])
 
     else:
 
-        print("Virtual Environment: None detected")
+        print("Virtual Environment: None detected\n")
         print("WARNING: You're in the global environment!")
-        print("The machines can see everything you install.")
+        print("The machines can see everything you install.\n")
         print("To enter the construct, run:")
         print("python -m venv matrix_env")
         print("source matrix_env/bin/activate  # On Unix")
-        print("matrix_env\\Scripts\\activate  # On Windows")
+        print("matrix_env\\Scripts\\activate  # On Windows\n")
+        print("Then run this program again.")
 
 if __name__ == "__main__":
     main()
